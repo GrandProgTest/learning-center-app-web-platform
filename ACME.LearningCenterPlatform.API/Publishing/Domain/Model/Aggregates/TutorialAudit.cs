44f1fd2f4b436e8e421bcbb430468b990995351f
GrandProgTest <u202124343@upc.edu.pe>
2024-05-23 16:51:39 -0500

    fix: changed TutorialAudit partial class name

--- ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Aggregates/TutorialAudit.cs
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Aggregates/TutorialAudit.cs
@@ -3,7 +3,7 @@ using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
 
 namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
 
-public partial class TutorialAudit : IEntityWithCreatedUpdatedDate
+public partial class Tutorial : IEntityWithCreatedUpdatedDate
 {
     [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
     [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
